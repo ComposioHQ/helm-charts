2555a335f8898fd7de48b621d0d81cc399bf0372
Utkarsh Dixit <utkarsh@composio.dev>
2026-06-11 14:19:37 +0530

    Revert "chore: remove frontend service from helm chart (#311)"
    This reverts commit b8f3974b2be471caf90412dd73069d6f4814bf79.

--- .github/scripts/generate-release-changelog.sh
+++ .github/scripts/generate-release-changelog.sh
@@ -408,6 +408,12 @@ extract_images_json() {
         "tag": (.mercury.image.tag // ""),
         "fallback_source_repo": "ComposioHQ/mercury"
       },
+      "frontend": {
+        "registry": (.global.registry.name // ""),
+        "repository": (.frontend.image.repository // ""),
+        "tag": (.frontend.image.tag // ""),
+        "fallback_source_repo": "ComposioHQ/frontend"
+      },
       "weaviate": {
         "registry": (.global.registry.name // ""),
         "repository": (.weaviate.image.repository // ""),
